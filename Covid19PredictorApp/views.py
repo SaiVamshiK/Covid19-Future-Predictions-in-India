977e615a0b5046b60842dffce06b610eb42949c0
SaiVamshi <saivamshi.k.24@gmail.com>
2021-04-30 17:58:18 +0530

    States Added

--- Covid19PredictorApp/views.py
+++ Covid19PredictorApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return render(request,'Covid19PredictorApp/base.html')
+    return render(request,'Covid19PredictorApp/home.html')
 
 def about(request):
     return render(request,'Covid19PredictorApp/about.html')
